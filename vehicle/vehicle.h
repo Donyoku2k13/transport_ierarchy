c46a3c3f6df972beb928e5f9adaa2471cd64d945
Sasha Goryachev <45205367+Donyoku2k13@users.noreply.github.com>
2019-05-31 18:29:46 +0600

    Update vehicle.h

--- vehicle/vehicle.h
+++ vehicle/vehicle.h
@@ -7,25 +7,26 @@ class vehicle
 {
 
 	string color;
+	string model;
 	size_t maxSpeed;
 	size_t cntPass;
 public:
+	vehicle(string model, string color) {
+		this->color = color;
+		this->model = model;
+	}
+	virtual void getInfo() {
+		cout << string(typeid(*this).name()).substr(6)
+			<< " " << color << " " << model << endl;
+	}
 	void setColor(string color) { this->color = color; }
 	string getColor()const { return this->color; }
 	void setMaxSpeed(size_t maxSpeed) { this->maxSpeed = maxSpeed; }
 	size_t getMaxSpeed()const { return this->maxSpeed; }
 	void setcntPass(size_t cntPass) { this->cntPass = cntPass; }
 	size_t getcntPass()const { return cntPass; }
-	virtual void getInfo()const { cout << getcntPass() << " " << getMaxSpeed() << endl; cout << getColor(); }
+	
 };
-
-
-
-
-
-
-
-
 
 
 
