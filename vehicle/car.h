89eb34ad0f6e6df6cd0bef49f3997df8d903a284
Sasha Goryachev <45205367+Donyoku2k13@users.noreply.github.com>
2019-05-31 18:28:07 +0600

    Update car.h

--- vehicle/car.h
+++ vehicle/car.h
@@ -1,15 +1,13 @@
 #pragma once
+#include "vehicle.h"
 #include "groundV.h"
-class car : public groundVehicle {
+class car : public vehicle {
 public:
+	car(string model, string color):vehicle(model,color) {
+		
+	}
 	void start() {
 		cout << "vehicle has started on the ground\n";
 	}
-	void  setBody(string body) { this->body = body; }
-	string getBody()const { return body; }
-	void setWheels(size_t wheel) { this->numofWheels = wheel; }
-	size_t getWheels()const { return numofWheels; }
-	void setEngine(string engine) { this->typeOfEngine = engine; }
-	string getEngineType()const { return typeOfEngine; }
-	void getInfo() override  { cout << getEngineType() << " " << getBody()<<" "<< getColor() << " " << getWheels() << endl; }
+	
 };
